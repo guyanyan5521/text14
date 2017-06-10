421281f7b56fb7b9d07dedbbd19c805cab1276c6
dell <guyanyan0503@163.com>
2017-06-10 14:31:03 +0800

    dom练习

---
+++ javascript/js1.js
@@ -0,0 +1,9 @@
+/**
+ * Created by dell on 2017/5/13.
+ */
+var today = new Date();   //获取当天日期
+var year = today.getFullYear();   //获取当天年份
+if(year%400==0){
+    Document.write("该年份是闰年");
+        }
+Document.write("该年份是闰年");
